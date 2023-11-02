cbb0d96266e0f9bd06eaf99fd56b9e9c43f85b13
Sam Heckle <samheckle@yahoo.com>
2023-11-02 12:37:34 -0400

    Add files via upload

---
+++ icm-example/sketch.js
@@ -0,0 +1,11 @@
+
+
+function setup() {
+  // put setup code here
+  createCanvas(window.innerWidth, window.innerHeight)
+  background('maroon')
+}
+
+function draw() {
+  // put drawing code here
+}
